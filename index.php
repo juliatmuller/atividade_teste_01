91c4db8d9fff85b88e26db145e2580fb77123299
juliatmuller <julia_t_muller@estudante.sesisenai.org.br>
2026-05-21 09:25:01 -0300

    add - PHP session

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+
+session_start();
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "sistema_simples";
+
+?> 
+
+
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,5 +20,16 @@
 </head>
 <body>
     <h1>Tela de Login - PHP</h1>
+
+    <form method="POST">
+
+    <label>Usuarios</label>
+    <input type="text" name="usuario"> <br>
+    <label>Senha</label>
+    <input type="password" name="senha"> <br>
+
+    <button type="submit"> Entrar </button> 
+  
+</form>
 </body>
 </html>
